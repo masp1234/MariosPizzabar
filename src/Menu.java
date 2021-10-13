87f59ac1d3900de881366ac3a468bddc40e9a66f
Chris <chri89c4@stud.kea.dk>
2021-10-13 14:28:19 +0200

    added Layout for menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,27 +1,28 @@
 import java.util.ArrayList;
 
 public class Menu {
-     ArrayList<Pizza> list;
-    Menu(){
+    ArrayList<Pizza> list;
+
+    Menu() {
         init();
     }
 
-    public void init(){
+    public void init() {
         list = new ArrayList<>();
-        Pizza pizza1 = new Pizza(1,"Vesuvio", "tomatsauce, ost, skinke og oregano", 57);
-        Pizza pizza2 = new Pizza(2,"Amerikaner", "tomatsauce, ost, oksefars og oregano", 53);
-        Pizza pizza3 = new Pizza(3,"Casiatore", " tomatsauce, ost, pepperoni og oregano", 57);
+        Pizza pizza1 = new Pizza(1, "Vesuvio", "tomatsauce, ost, skinke og oregano", 57);
+        Pizza pizza2 = new Pizza(2, "Amerikaner", "tomatsauce, ost, oksefars og oregano", 53);
+        Pizza pizza3 = new Pizza(3, "Casiatore", " tomatsauce, ost, pepperoni og oregano", 57);
         Pizza pizza4 = new Pizza(4, "Carbonara", "tomatsauce, ost, kodsauce, spaghetti, cocktailpølser og oregano", 63);
-        Pizza pizza5 = new Pizza (5, "Dennis", "tomaatsauce, ost, skinke, pepperoni, cocktailpølser og oregano", 65);
-        Pizza pizza6 = new Pizza (6, "Bertil", "tomatsauce, ost, bacon, oregano", 57);
-        Pizza pizza7 = new Pizza (7, "Silvia", "tomatsauce, ost, pepperoni, rød peber, løg, oliven og oregano", 61);
-        Pizza pizza8 = new Pizza (8, "Victoria", "tomatsauce, ost, skinke, ananas, champignon, løg og oregano", 61);
-        Pizza pizza9 = new Pizza (9, "Toronfo", "tomatsauce, ost, skinke, bacon, kebab, chili og oregano",61);
-        Pizza pizza10 = new Pizza (10, "Capricciosa", "tomatsauce, ost, skinke, champignon og oregano",61);
-        Pizza pizza11 = new Pizza (11, "Hawai", "tomatsauce, ost, skinke, ananas og oregano", 61);
-        Pizza pizza12 = new Pizza (12, "Le Blissola", "tomatsauce, ost, skinke, rejer og oregano", 61);
-        Pizza pizza13 = new Pizza (13, "Venezia", "tomatsauce, ost, skinke, bacon og oregano", 61);
-        Pizza pizza14 = new Pizza (14, "Mafia", "tomatsauce, ost, pepperoni, bacon, løg, oregano", 61);
+        Pizza pizza5 = new Pizza(5, "Dennis", "tomaatsauce, ost, skinke, pepperoni, cocktailpølser og oregano", 65);
+        Pizza pizza6 = new Pizza(6, "Bertil", "tomatsauce, ost, bacon, oregano", 57);
+        Pizza pizza7 = new Pizza(7, "Silvia", "tomatsauce, ost, pepperoni, rød peber, løg, oliven og oregano", 61);
+        Pizza pizza8 = new Pizza(8, "Victoria", "tomatsauce, ost, skinke, ananas, champignon, løg og oregano", 61);
+        Pizza pizza9 = new Pizza(9, "Toronfo", "tomatsauce, ost, skinke, bacon, kebab, chili og oregano", 61);
+        Pizza pizza10 = new Pizza(10, "Capricciosa", "tomatsauce, ost, skinke, champignon og oregano", 61);
+        Pizza pizza11 = new Pizza(11, "Hawai", "tomatsauce, ost, skinke, ananas og oregano", 61);
+        Pizza pizza12 = new Pizza(12, "Le Blissola", "tomatsauce, ost, skinke, rejer og oregano", 61);
+        Pizza pizza13 = new Pizza(13, "Venezia", "tomatsauce, ost, skinke, bacon og oregano", 61);
+        Pizza pizza14 = new Pizza(14, "Mafia", "tomatsauce, ost, pepperoni, bacon, løg, oregano", 61);
 
 
         list.add(pizza1);
@@ -38,9 +39,30 @@ public class Menu {
         list.add(pizza12);
         list.add(pizza13);
         list.add(pizza14);
+
+        /*
+        System.out.println("MARIOS PIZZABAR");
+        System.out.println("____________________________________________");
+        System.out.println("Vælg en af følgende Pizzaer:");
+        System.out.println("Nr.1, Vesuvio: Tomatsauce, ost, skinke og oregano.................................57 kr.");
+        System.out.println("Nr. 2, Amerikaner: Tomatsauce, ost, oksefars og oregano............................53 kr.");
+        System.out.println("Nr. 3, Casiatore: Tomatsauce, ost, pepperoni og oregano............................57 kr.");
+        System.out.println("Nr. 4, Carbonara: Tomatsauce, ost, kodsauce, spaghetti, cocktailpølser og oregano..63 kr.");
+        System.out.println("Nr. 5, Dennis: Tomaatsauce, ost, skinke, pepperoni, cocktailpølser og oregano......65 kr.");
+        System.out.println("Nr. 6, Bertil: Tomatsauce, ost, bacon, oregano.....................................57 kr.");
+        System.out.println("Nr. 7, Silvia: Tomatsauce, ost, pepperoni, rød peber, løg, oliven og oregano.......61 kr.");
+        System.out.println("Nr. 8, Victoria: Tomatsauce, ost, skinke, ananas, champignon, løg og oregano.......61 kr.");
+        System.out.println("Nr. 9, Toronfo: Tomatsauce, ost, skinke, bacon, kebab, chili og oregano............61 kr.");
+        System.out.println("Nr. 10, Capricciosa: Tomatsauce, ost, skinke, champignon og oregano................61 kr.");
+        System.out.println("Nr. 11, Hawai: Tomatsauce, ost, skinke, ananas og oregano..........................61 kr.");
+        System.out.println("Nr. 12, Le Blissola: Tomatsauce, ost, skinke, rejer og oregano.....................61 kr.");
+        System.out.println("Nr. 13, Venezia: Tomatsauce, ost, skinke, bacon og oregano.........................61 kr.");
+        System.out.println("Nr. 14, Mafia: Tomatsauce, ost, pepperoni, bacon, løg, oregano.....................61 kr.");
+        */
+
     }
 
-    public void printList(){
+    public void printList() {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
 
@@ -50,7 +72,5 @@ public class Menu {
     public ArrayList<Pizza> getList() {
         return list;
     }
-
-
-
 }
+
--- src/OrderList.java
+++ src/OrderList.java
@@ -1,25 +1,29 @@
 import java.util.ArrayList;
 
 public class OrderList {
-     ArrayList<Pizza> orderList = new ArrayList<>();
+  ArrayList<Pizza> orderList = new ArrayList<>();
 
 
-
-   public void printList(){
-       for (int i = 0; i < orderList.size(); i++) {
-           System.out.println(orderList.get(i));
-       }
+  public void printList() {
+    for (int i = 0; i < orderList.size(); i++) {
+      System.out.println(orderList.get(i));
     }
-    public void addPizza(int pizzaNumber){
+  }
 
-    }
-    public void addPizza(String pizzaName){
+  public void addPizza(int pizzaNumber) {
 
-    }
-    public void removePizza(String pizzaName){
+  }
 
-    }
-    public void removePizza(int pizzaNumber){
+  public void addPizza(String pizzaName) {
 
-    }
+  }
+
+  public void removePizza(String pizzaName) {
+
+  }
+
+  public void removePizza(int pizzaNumber) {
+
+  }
 }
+
