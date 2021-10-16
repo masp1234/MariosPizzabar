dd780c31be9ffb690a0788942ceb2a0222a1f167
Daniel Koltze <djoe@itu.dk>
2021-10-16 11:19:52 +0200

    order

--- src/Order.java
+++ src/Order.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Order {
     ArrayList<Pizza> listOfPizza = new ArrayList<>();
     Menukort menukort = new Menukort();
-
+    //adder pizza
     public void addPizza(int pizzaNumber) {
         listOfPizza.add(menukort.getList().get(pizzaNumber - 1));
     }
